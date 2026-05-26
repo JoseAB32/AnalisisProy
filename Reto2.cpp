480c36a14f11049f180107fa900ab50ab34db146
LokoAddams <lucasalcoba74@gmail.com>
2026-05-26 07:38:12 -0400

    a

--- Reto2.cpp
+++ Reto2.cpp
@@ -41,33 +41,47 @@ unordered_map<int, vector<int>> invertir_grafo(unordered_map<int, vector<int>> &
   return grafo_invertido;
 }
 
+int maxSCC = -1; vector<int> scc_tamanio;
 int dfs2(unordered_map<int, vector<int>> &grafo_invertido, int maxId)
 {
   reverse(st.begin(), st.end());
+
+  scc_tamanio.assign(maxId + 1, 0);
   visitados.assign(maxId + 1, false);
-  int max = st.size(), cont = 0;
-  form(i,0,max)
+  int maxi = st.size(), cont = 0;
+  form(i,0,maxi)
     if(visitados[st[i]] == false )
-      dfs(st[i],grafo_invertido,cont), cont++;
+    {
+      int anterior = st.size();
+      dfs(st[i],grafo_invertido,cont);
+      scc_tamanio[cont] = ((int)st.size()) - anterior;
+      cont++;
+      maxSCC = max(maxSCC,((int)st.size()) - anterior);
+    }
   return cont;
 }
 
+int maxAislado = -1;
 int contarGruposAislados(unordered_map<int, vector<int>> &grafo)
 {
   unordered_map<int, bool> gruposAislados;
   for(auto par: grafo)
   {
     auto [i , vec] = par;
-    if(gruposAislados.find(i) == gruposAislados.end())
-      gruposAislados[i] = true;
+    int grupo = scc[i];
+    if(gruposAislados.find(grupo) == gruposAislados.end())
+      gruposAislados[grupo] = true;
     for(auto e: vec)
-      if(scc[e] != scc[i])
-        gruposAislados[i] = gruposAislados[e] = false;
+      if(scc[e] != grupo)
+        gruposAislados[grupo] = gruposAislados[scc[e]] = false;
   }
   int cont = 0;
   for(auto par: gruposAislados)
     if(par.second)
+    {
+      maxAislado = max(maxAislado, scc_tamanio[par.first]);
       cont++;
+    }
   return cont;
 }
 
@@ -85,13 +99,10 @@ int main()
   unordered_map<int, vector<int>> grafo_invertido = invertir_grafo(grafo, maxId);
   
   cout <<"Componentes fuertemente conexas: " <<dfs2(grafo_invertido, maxId) << endl;
+  cout <<"Componente fuertemente conexa mas grande: " << maxSCC << endl;
 
-  
-
-  
-  // cout <<"Componente fuertemente conexa mas grande: " << maxSCC << endl;
 
   cout<<"Grupos Aislados: " << contarGruposAislados(grafo) << endl;
-  // cout<<"Grupo Aislado mas grande: " << maxAislado << endl;
+  cout<<"Grupo Aislado mas grande: " << maxAislado << endl;
   return 0;
 }
